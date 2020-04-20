a68fb4814370968d1ec4e7a9a0858b46a746f379
Monika Kowalska <m.kowalska@futureit.agency>
2020-04-20 03:57:28 +0200

    add animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-import '../scss/style.scss';
+// import '../scss/style.scss';
+
+window.onload = function() {
+  lax.setup()
+  const update = () => {
+    lax.update(window.scrollY)
+    window.requestAnimationFrame(update)
+  }
+  window.requestAnimationFrame(update)
+  window.addEventListener("scroll", function() {
+    lax.updateElements()
+  });
+}
